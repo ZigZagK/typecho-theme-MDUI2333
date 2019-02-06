539bd1fdfcea94e0c7e81239839de8b157d57e78
ZigZagK <744388629@qq.com>
2019-02-06 09:30:09 +0800

    更新至1.1.7版本

--- functions.php
+++ functions.php
@@ -129,7 +129,7 @@ function AddMDUITable($content){
 	return preg_replace('/<\/table>/s','</table></div>',preg_replace('/<table>/s','<div class="mdui-table-fluid"><table class="mdui-table mdui-table-hoverable">',$content));
 }
 function AddFancybox($content){
-	return preg_replace('/<img(.*?)src="(.*?)"(.*?)>/s','<a data-fancybox="gallery" href="${2}" class="Fancybox"><img${1}src="${2}"${3}></a>',$content);
+	return preg_replace('/<img(.*?)src="(.*?)"(.*?)alt="(.*?)"(.*?)>/s','<a data-fancybox="gallery" href="${2}" data-caption="${4}" class="Fancybox"><img${1}src="${2}"${3}alt="${4}"${5}></a>',$content);
 }
 function RewriteContent($content){
 	return AddMDUITable(AddFancybox($content));
