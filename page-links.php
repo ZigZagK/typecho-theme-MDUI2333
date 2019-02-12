0ea81baa91ecc78059f8d8a75621b08a0c3f99a2
ZigZagK <744388629@qq.com>
2019-02-13 01:43:51 +0800

    Update:用MDUI重写友情链接页面

--- page-links.php
+++ page-links.php
@@ -6,106 +6,24 @@
  */
 $this->need('header.php'); ?>
 
-<style>
-	.md-links {
-		min-height: calc(100% - 120px - 5pc - 6em);
-		text-align: center;
-		overflow: auto;
-		padding: 0;
-		margin: 0 auto;
-		max-width: 320px;
-	}
-
-	@media screen and (min-width: 680px) {
-		.md-links {
-			max-width: 640px;
-		}
-	}
-
-	@media screen and (min-width: 1000px) {
-		.md-links {
-			max-width: 960px;
-		}
-	}
-
-	@media screen and (min-width: 1320px) {
-		.md-links {
-			max-width: 1280px;
-		}
-	}
-
-	@media screen and (min-width: 1640px) {
-		.md-links {
-			max-width: 1600px;
-		}
-	}
-
-	@media screen and (max-width: 480px) {
-		.md-links {
-			min-height: calc(100% - 200px - 5pc - 6em);
-			margin-top: 6em;
-		}
-	}
-
-	.md-links-item {
-		background: #fff;
-		box-shadow: 0 2px 2px 0 rgba(0, 0, 0, .14), 0 3px 1px -2px rgba(0, 0, 0, .2), 0 1px 5px 0 rgba(0, 0, 0, .12);
-		height: 72px;
-		line-height: 15px;
-		margin: 20px 10px;
-		padding: 0px 0px;
-		transition: box-shadow 0.25s;
-	}
-
-	.md-links a {
-		color: #333;
-		text-decoration: none;
-	}
-
-	.md-links li {
-		width: 300px;
-		float: left;
-		list-style: none;
-	}
-
-	.md-links-item img {
-		float: left;
-		box-shadow: 0 2px 2px 0 rgba(0, 0, 0, .14), 0 3px 11px -2px rgba(0, 0, 0, .2), 0 1px 5px 0 rgba(0, 0, 0, .12);
-	}
-
-	.md-links-item:hover {
-		box-shadow: 0 4px 10px rgba(0, 0, 0, 0.3);
-	}
-
-	.md-links-item a:hover{
-		cursor: pointer;
-	}
-
-	.md-links-title {
-		font-size: 20px;
-		line-height: 50px;
-	}
-
-	#scheme-Paradox .mdl-mini-footer{
-		clear: left;
-	}
-	#bottom{
-		position: relative;
-	}
-</style>
-
-<div class="md-links">
-	<?php if (class_exists("Links_Plugin")): ?>
-		<?php Links_Plugin::output('
-		<a href="{url}" target="_blank">
-			<li class="md-links-item">
-				<img src="{image}" alt="{name}" height="72px"/>
-				<span  class="md-links-title">{name}</span><br />
-				<span>{description}</span>
-			</li>
-		</a>
-		'); ?>
-	<?php endif; ?>
+<div class="mdui-container">
+	<div class="mdui-row-xs-1 mdui-row-sm-2 mdui-row-md-3 mdui-row-lg-4 mdui-row-xl-5 mdui-m-y-2">
+		<?php if (class_exists("Links_Plugin")): ?>
+			<?php Links_Plugin::output('
+			<div class="mdui-col">
+				<div class="mdui-card mdui-m-y-1 mdui-hoverable">
+					<a href="{url}" target="_blank">
+						<div class="mdui-card-header">
+							<img class="mdui-card-header-avatar" src="{image}" alt="{name}"/>
+							<div class="mdui-card-header-title">{name}</div>
+							<div class="mdui-card-header-subtitle">{description}</div>
+						</div>
+					</a>
+				</div>
+			</div>
+			'); ?>
+		<?php endif; ?>
+	</div>
 </div>
 
 <?php include('sidebar.php'); ?>
